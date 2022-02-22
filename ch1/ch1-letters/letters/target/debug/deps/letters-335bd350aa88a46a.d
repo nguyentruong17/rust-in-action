fd25888aca76e5c391dd580beca1f9775fb28f43
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2022-02-21 23:26:23 -0600

    Delete ch1/ch1-letters/letters/target directory

--- ch1/ch1-letters/letters/target/debug/deps/letters-335bd350aa88a46a.d
+++
@@ -1,5 +0,0 @@
-/home/nguyen/Codes/rust-in-action/ch1/ch1-letters/letters/target/debug/deps/letters-335bd350aa88a46a.rmeta: src/main.rs
-
-/home/nguyen/Codes/rust-in-action/ch1/ch1-letters/letters/target/debug/deps/letters-335bd350aa88a46a.d: src/main.rs
-
-src/main.rs:
