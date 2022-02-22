2b490afdc7c0fdd1c1b9bfd58d95062657e323cf
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2022-02-21 23:26:09 -0600

    Delete ch1/ch1-fruit/fruits/target directory

--- ch1/ch1-fruit/fruits/target/debug/deps/fruits-a4b6f4fb4fcc733c.d
+++
@@ -1,5 +0,0 @@
-/home/nguyen/Codes/rust-in-action/ch1/ch1-fruit/fruits/target/debug/deps/fruits-a4b6f4fb4fcc733c.rmeta: src/main.rs
-
-/home/nguyen/Codes/rust-in-action/ch1/ch1-fruit/fruits/target/debug/deps/fruits-a4b6f4fb4fcc733c.d: src/main.rs
-
-src/main.rs:
--- ch1/ch1-fruit/fruits/target/debug/fruits.d
+++
@@ -1,1 +0,0 @@
-/home/nguyen/Codes/rust-in-action/ch1/ch1-fruit/fruits/target/debug/fruits: /home/nguyen/Codes/rust-in-action/ch1/ch1-fruit/fruits/src/main.rs
