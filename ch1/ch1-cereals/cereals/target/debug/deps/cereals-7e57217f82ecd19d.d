7c337c4ceabdb56bbac4abfc46f264a0cd360862
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2022-02-21 23:25:47 -0600

    Delete ch1/ch1-cereals/cereals/target directory

--- ch1/ch1-cereals/cereals/target/debug/deps/cereals-7e57217f82ecd19d.d
+++
@@ -1,5 +0,0 @@
-/home/nguyen/Codes/rust-in-action/ch1/ch1-cereals/cereals/target/debug/deps/cereals-7e57217f82ecd19d.rmeta: src/main.rs
-
-/home/nguyen/Codes/rust-in-action/ch1/ch1-cereals/cereals/target/debug/deps/cereals-7e57217f82ecd19d.d: src/main.rs
-
-src/main.rs:
