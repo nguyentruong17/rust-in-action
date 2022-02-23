414d01dcdc8a51d9ae085be618861b8eecfc00d8
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2022-02-22 23:53:59 -0600

    Delete ch2/ch2-first-steps/target directory

--- ch2/ch2-first-steps/target/debug/deps/ch2_first_steps-bf9c18fe1bfcdbdb.d
+++
@@ -1,5 +0,0 @@
-/home/nguyen/Codes/rust-in-action/ch2/ch2-first-steps/target/debug/deps/ch2_first_steps-bf9c18fe1bfcdbdb.rmeta: src/main.rs
-
-/home/nguyen/Codes/rust-in-action/ch2/ch2-first-steps/target/debug/deps/ch2_first_steps-bf9c18fe1bfcdbdb.d: src/main.rs
-
-src/main.rs:
